436c39fa81961532cb280ef768851d9b3dbd6c77
Amansharma4508 <amanshrma2989@gmail.com>
2026-07-02 17:01:39 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-blue-600 p-10">
+      Healthcare Dashboard
+    </div>
+  );
+}
+
+export default App;
